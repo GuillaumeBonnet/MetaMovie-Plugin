9afd77d1bd7b7b770850e45793ddc8a38e15acec
Guillaume Bonnet <7252481+GuillaumeBonnet@users.noreply.github.com>
2020-11-07 22:51:10 +0100

    waiting for loading before inserting vue and the components

--- src/chrome-scripts/content.ts
+++ src/chrome-scripts/content.ts
@@ -1,20 +1,69 @@
 declare const chrome: any;
 
-const videoTitle = document.querySelector('.video-title');
-if (videoTitle) {
-	const containerDiv = document.createElement('div');
-	containerDiv.className =
-		'touchable PlayerControls--control-element nfp-popup-control';
-
-	const appDiv = document.createElement('div') as HTMLElement;
-	appDiv.id = 'plugin-meta-movie-menu';
-
-	containerDiv.appendChild(appDiv);
+interface IAwaitedNodes {
+	videoTitle: Element | null;
+	video: Element | null;
+	videoWrapper: Element | null;
 }
 
-const videoWrapper = document.querySelector('div.sizing-wrapper');
-if (videoWrapper) {
+const maxLoadingTime = 5000,
+	initialTimestamp = Date.now(); //ms
+const intervalId = setInterval(() => {
+	const awaitedNodes = getAwaitedNode();
+	if (awaitedNodes) {
+		insertPluginNodes(awaitedNodes);
+		loadScripts();
+		clearInterval(intervalId);
+	} else if (Date.now() - initialTimestamp > maxLoadingTime) {
+		clearInterval(intervalId);
+		throw Error('Video not found or did not load in time.');
+	}
+}, 200);
+
+function getAwaitedNode() {
+	const awaitedNodes: IAwaitedNodes = {
+		videoTitle: document.querySelector('.video-title'),
+		video: document.querySelector('video'),
+		videoWrapper: document.querySelector('div.sizing-wrapper'),
+	};
+	for (const awaitedNodeKey of Object.keys(awaitedNodes)) {
+		if (!awaitedNodes[awaitedNodeKey as keyof IAwaitedNodes]) {
+			return false;
+		}
+	}
+	return awaitedNodes;
+}
+
+function insertPluginNodes(awaitedNodes: IAwaitedNodes) {
+	const menuDiv = document.createElement('div') as HTMLElement;
+	menuDiv.id = 'plugin-meta-movie-menu';
+
+	const containerMenuDiv = document.createElement('div');
+	containerMenuDiv.className =
+		'touchable PlayerControls--control-element nfp-popup-control';
+	containerMenuDiv.appendChild(menuDiv);
+
+	awaitedNodes.videoTitle?.after(containerMenuDiv);
+
 	const videoOverlayDiv = document.createElement('div') as HTMLElement;
 	videoOverlayDiv.id = 'plugin-meta-movie-video-overlay';
-	videoWrapper.prepend(videoOverlayDiv);
+	awaitedNodes.videoWrapper?.prepend(videoOverlayDiv);
+}
+
+function loadScripts() {
+	const scriptNames = [
+		'vue.js',
+		'js/chunk-vendors.js',
+		'js/content-overlay.js',
+	];
+	const body = document.querySelector('body');
+	if (!body) {
+		throw Error('no body node.');
+	}
+	for (const name of scriptNames) {
+		const script = document.createElement('script');
+		const url = chrome.runtime.getURL(`/${name}`);
+		script.setAttribute('src', url);
+		body.appendChild(script);
+	}
 }
