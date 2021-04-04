1f9245cf5aba35297af9fd42cef49ac25e9e4ff9
Guillaume Bonnet <7252481+GuillaumeBonnet@users.noreply.github.com>
2021-04-04 22:52:30 +0200

    deck name in popup msg

--- src/components/DeckSelector/DeckSelector.vue
+++ src/components/DeckSelector/DeckSelector.vue
@@ -4,7 +4,10 @@
 			ref="popup-deck-selector-modifications"
 			title="Deck has unsaved modifications."
 		>
-			Deck has unsaved modifications
+			<div>
+				Deck <span class="italic">{{ currentDeck?.name }}</span> has unsaved
+				modifications
+			</div>
 			<template v-slot:actions>
 				<MatButton
 					label="Cancel"
@@ -51,38 +54,6 @@
 			</button>
 			<NewDeck class="my-8"></NewDeck>
 			<div class="underline mb-4 mt-8">Available decks:</div>
-			<MatPopup
-				ref="popup-deck-selector-modifications"
-				title="Deck has unsaved modifications."
-			>
-				Deck has unsaved modifications
-				<template v-slot:actions>
-					<button
-						type="button"
-						class="mdc-button mdc-button--outlined mdc-dialog__button"
-						data-mdc-dialog-action="cancel"
-					>
-						<div class="mdc-button__ripple"></div>
-						<span class="mdc-button__label">Cancel</span>
-					</button>
-					<button
-						type="button"
-						class="mdc-button mdc-button--outlined mdc-dialog__button"
-						data-mdc-dialog-action="discard"
-					>
-						<div class="mdc-button__ripple"></div>
-						<span class="mdc-button__label">Discard ?</span>
-					</button>
-					<button
-						type="button"
-						class="mdc-button mdc-button--outlined mdc-dialog__button"
-						data-mdc-dialog-action="save"
-					>
-						<div class="mdc-button__ripple"></div>
-						<span class="mdc-button__label">Save ?</span>
-					</button>
-				</template>
-			</MatPopup>
 			<table class=" w-full text-3xl table-auto border-1 border-black">
 				<thead class="text-gray-900 bg-gray-500 border-b-1 border-black">
 					<tr>
