874ea09e64996fff737b24b9dd154f2e36f6a229
Guillaume Bonnet <7252481+GuillaumeBonnet@users.noreply.github.com>
2021-04-30 02:30:36 +0200

    withCredentials with interceptors

--- src/Utils/WebService.ts
+++ src/Utils/WebService.ts
@@ -6,55 +6,47 @@ import {
 } from '@/models/ApiTypes';
 import { DeckData } from '@/models/DeckData';
 import axios, { AxiosRequestConfig } from 'axios';
-
+axios.interceptors.request.use(config => {
+	config.withCredentials = true;
+	return config;
+});
 const rootUrl = process.env.VUE_APP_API_URL;
-const axiosConfig: AxiosRequestConfig = {
-	withCredentials: true,
-};
 const fetchCompleteDeck = (deck: DeckData) => {
-	return axios.get<DeckApi>(`${rootUrl}/decks/${deck.id}`, axiosConfig);
+	return axios.get<DeckApi>(`${rootUrl}/decks/${deck.id}`);
 };
 
 const fetchAllDecks = () => {
-	return axios.get<DeckApi_WithoutCards[]>(`${rootUrl}/decks`, axiosConfig);
+	return axios.get<DeckApi_WithoutCards[]>(`${rootUrl}/decks`);
 };
 
 const saveDeck = async (deck: CreateFields<DeckApi>) => {
-	return axios.post<DeckApi>(`${rootUrl}/decks`, deck, axiosConfig);
+	return axios.post<DeckApi>(`${rootUrl}/decks`, deck);
 };
 const updateDeck = (deck: DeckData) => {
-	return axios.put<DeckApi>(`${rootUrl}/decks/${deck.id}`, deck, axiosConfig);
+	return axios.put<DeckApi>(`${rootUrl}/decks/${deck.id}`, deck);
 };
 const deleteDeck = (deckId: number) => {
-	return axios.delete<any>(`${rootUrl}/decks/${deckId}`, axiosConfig);
+	return axios.delete<any>(`${rootUrl}/decks/${deckId}`);
 };
 const logout = () => {
-	return axios.get(`${rootUrl}/users/logout`, axiosConfig);
+	return axios.get(`${rootUrl}/users/logout`);
 };
 const login = (credentials: { email: string; password: string }) => {
-	return axios.post<UserInfo>(
-		`${rootUrl}/users/login`,
-		{
-			...credentials,
-		},
-		axiosConfig
-	);
+	return axios.post<UserInfo>(`${rootUrl}/users/login`, {
+		...credentials,
+	});
 };
 const signUp = (credentials: {
 	email: string;
 	username: string;
 	password: string;
 }) => {
-	return axios.post<UserInfo>(
-		`${rootUrl}/users/signup`,
-		{
-			...credentials,
-		},
-		axiosConfig
-	);
+	return axios.post<UserInfo>(`${rootUrl}/users/signup`, {
+		...credentials,
+	});
 };
 const userInfo = () => {
-	return axios.get<UserInfo>(`${rootUrl}/users/info`, axiosConfig);
+	return axios.get<UserInfo>(`${rootUrl}/users/info`);
 };
 export {
 	fetchCompleteDeck,
